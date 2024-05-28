5b6d1a7320479ffbed978f6cdf438bfceb7adb8b
yakuphantonintshi <yakupha58756@gmail.com>
2024-05-28 16:23:57 +0200

    Add File

---
+++ script/code.js
@@ -0,0 +1,15 @@
+//Question 1
+let data = ['Yakupha', 4, 6, 9, 11, 'Ntonintshi', 21]
+console.log(data)
+
+//Question 2
+
+
+//Question 4
+console.log(data.find(value => value == 11));
+
+//Question 5
+console.log(data.findIndex(value => value == 11))
+
+//Question 6
+
